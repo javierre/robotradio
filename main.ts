12b563eb281b3db2f8691482a5e713bb02076a83
javierre <javierperezlara@gmail.com>
2022-02-06 18:41:42 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,21 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
+    } else if (receivedNumber == 2) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 150)
+    } else if (receivedNumber == 3) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 150)
+    } else if (receivedNumber == 4) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 150)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 150)
+    } else {
+    	
+    }
+})
 radio.setGroup(1)
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+strip.showColor(neopixel.colors(NeoPixelColors.Green))
 basic.forever(function () {
 	
 })
